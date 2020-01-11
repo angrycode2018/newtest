541fd84637c8e1bf0048fb44c5a0b72fa626a6b0
angrycode2018 <47102718+angrycode2018@users.noreply.github.com>
2020-01-11 13:34:58 +0700

    friend

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+// Friends made comment
 router.get('/', function(req, res, next) {
   console.log(req)
   res.send('This is respond from Express');
