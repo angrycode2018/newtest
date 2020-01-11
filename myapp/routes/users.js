5f436020cbf18023a9defcabbbe8a22fefcd15ca
angrycode2018 <angrycode2018@gmail.com>
2020-01-11 14:30:22 +0700

    Ohm commit after

--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-//todo GET users listing. */
-// Ohm made this comment
-/* GET users listing. */
-// Friends made comment
+
+// Ohm commit after
+
 router.get('/', function(req, res, next) {
   //* console.log(req)
   res.send('This is respond from Express');
